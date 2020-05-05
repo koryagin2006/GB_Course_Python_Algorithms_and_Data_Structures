791da54a3323693e16a78f561e6d7798c65851c4
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2020-05-06 02:34:24 +0500

    Update task_6.py

--- Les_4 Эмпирическая оценка алгоритмов/task_6.py
+++ Les_4 Эмпирическая оценка алгоритмов/task_6.py
@@ -12,12 +12,12 @@ def fib_loop(n):
     if n < 2:
         return n
     first, second = 0, 1
-    for i in range(2, n + 1):
+    for _ in range(2, n + 1):
         first, second = second, first + second
     return second
 
 
-# test_fib(fib_loop)
+test_fib(fib_loop)
 
 # task_6.fib_loop(10)
 # 1000 loops, best of 5: 1.7 usec per loop
