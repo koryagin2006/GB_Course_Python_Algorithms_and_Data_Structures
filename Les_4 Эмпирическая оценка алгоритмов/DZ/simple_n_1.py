138765485b3f7a1cd1001e93c367927dd10a68fa
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2020-05-07 00:03:52 +0500

    Create simple_n_1.py

---
+++ Les_4 Эмпирическая оценка алгоритмов/DZ/simple_n_1.py
@@ -0,0 +1,19 @@
+import cProfile
+
+# Решето Эратосфена
+
+n = int(input('До какого числа получить простые числа: '))
+
+
+def sito(n):
+    sieve = [i for i in range(n)]
+    sieve[1] = 0
+    for i in range(2, n):
+        if sieve[i] != 0:
+            j = i * 2
+            while j < n:
+                sieve[j] = 0
+                j += i
+    result = [i for i in sieve if i != 0]
+    # print(result)
+
