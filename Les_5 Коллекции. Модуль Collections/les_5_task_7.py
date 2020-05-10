445cca126d7b1e69c222779a72e4938e5603ae96
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2020-05-11 02:40:07 +0500

    Update les_5_task_7.py

--- Les_5 Коллекции. Модуль Collections/les_5_task_7.py
+++ Les_5 Коллекции. Модуль Collections/les_5_task_7.py
@@ -4,3 +4,9 @@ from collections import ChainMap
 # у нас будет возможность построить из них цепочку
 d1 = {'a': 2, 'b': 4, 'c': 5}
 d2 = {'a': 10, 'b': 20, 'd': 40}
+
+d_map = ChainMap(d1, d2)  # важен порядок элементов
+print(d_map)
+
+d2['a'] = 100
+print(d_map)
