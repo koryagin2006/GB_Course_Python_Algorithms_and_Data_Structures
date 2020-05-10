0844dfb9809b031e468ca94488c9f3dd3b2f620b
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2020-05-10 12:49:57 +0500

    Update les_5_task_5.py

--- Les_5 Коллекции. Модуль Collections/les_5_task_5.py
+++ Les_5 Коллекции. Модуль Collections/les_5_task_5.py
@@ -12,5 +12,27 @@ from collections import OrderedDict, defaultdict, deque
 N = 3000
 with open('big_log.txt', 'r', encoding='utf-8') as f:
     log = deque(f, N)  # сохраняем в очередь последние N значений
-    
 
+print(log)
+
+# объявляем переменные
+data = OrderedDict()
+spam = defaultdict(int)
+
+# избавляемся от символа перенова строки
+for item in log:
+    ip = item[:-1]  # для каждой строки оставляем все, кроме последнего символа
+
+    if not ip.startswith('192.168'):
+        spam[ip] += 1
+        data[ip] = 1
+
+print(spam)
+print(data)
+
+data.update(spam)  # добавляет в `data` ключи из `spam`
+print(data)
+
+with open ('data.txt', 'w', encoding='utf-8') as f:
+    for key, value in data.items():
+        f.write(f'{key} - {value}\n')
