059952496c96d07d3b602f88c9061b404e0f9469
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2020-06-04 23:46:35 +0500

    Update vremenniy.py

--- Les_7 Алгоритмы сортировки/vremenniy.py
+++ Les_7 Алгоритмы сортировки/vremenniy.py
@@ -1,32 +1,18 @@
-# task_4
+"""
+Массив размером 2m + 1, где m — натуральное число, заполнен случайным образом. 
+Найдите в массиве медиану. Медианой называется элемент ряда, делящий его на две равные части: 
+в одной находятся элементы, которые не меньше медианы, в другой — не больше медианы.
+
+Примечание: задачу можно решить без сортировки исходного массива. 
+Но если это слишком сложно, используйте метод сортировки, который не рассматривался 
+на уроках (сортировка слиянием также недопустима).
+"""""
 import random
 
-size = 10
+m = random.randint(0, 100)
+size = 2 * m + 1
+print(size)
+
 array = [i for i in range(size)]
 random.shuffle(array)
-print(array)
-
-
-def shell_sort(array):
-    assert len(array) < 4000
-    'Массив слишком большой. Используйте другую сортировку'
-
-    def new_increment(array):
-        inc = [1, 4, 10, 23, 57, 132, 301, 701, 1750]
-        while len(array) <= inc[-1]:
-            inc.pop()
-
-        while len(inc) > 0:
-            yield inc.pop()
-
-    for increment in new_increment(array):
-        for i in range(increment, len(array)):
-            for j in range(i, increment - 1, -increment):
-                if array[j - increment] <= array[j]:
-                    break
-                array[j], array[j - increment] = array[j - increment], array[j]
-                # print(array)
-
-
-shell_sort(array)
 print(array)
