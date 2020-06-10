5e5ace6875d11e751a84e1647e6066828bf41e1a
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2020-06-10 23:27:06 +0500

    Update vremenniy.py

--- Les_9 Деревья. Хеш-функции/vremenniy.py
+++ Les_9 Деревья. Хеш-функции/vremenniy.py
@@ -1,26 +1,16 @@
-# task_2
-from binarytree import bst
+import hashlib
 
 
-def search(bin_search_tree, number, path=''):
+def Rabin_Karp(s: str, subs: str) -> int:
     """
-    :param bin_search_tree: дерево
-    :param number: число
-    :param path: нужно для сохранения пути
+
+    :param s:
+    :param subs:
+    :return:
     """
-    if bin_search_tree.value == number:
-        return f'Число {number} обнаружено по следующему пути: \nКорень{path}'
-
-    if number < bin_search_tree.value and bin_search_tree.left is not None:
-        return search(bin_search_tree.left, number, path=f'{path}\nШаг влево')
-
-    return f'Число {number} отсутствует в дереве'
+    pass
 
 
-bt = bst(height=5, is_perfect=False)
-print(bt)
-num = int(input('Введите число для поиска: '))
-print(search(
-    bin_search_tree=bt,
-    number=num
-))
+s_1 = input('Введите строку: ')
+s_2 = input('Введите подстроку для поиска: ')
+
